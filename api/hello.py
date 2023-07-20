59e394e2420c741ef943f14430bfc2dd7c58c43e
Fabian Meyer <meyerfabian1988@gmail.com>
2023-07-20 16:58:05 +0200

    again a try

--- api/hello.py
+++ api/hello.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
